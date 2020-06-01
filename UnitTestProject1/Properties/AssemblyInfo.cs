19726f46d4a03d251061f9c66c16c63d176782f3
PolyRibina <poly.ribina@gmail.com>
2020-06-01 10:04:20 +0300

    File

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("743d2620-1b11-47fc-a5e8-0a81ad3b5e75")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string actError, expError = "Нет корней";
+            double[] expResult = new double[0];
+            int a = 2, b = 6, c = 9;
+            double[] actResult = Class1.Mathing(a, b, c, out actError);
+            Assert.AreEqual(expError, actError, "Полученное сообщение ошибке (" + actError + ") не совпадает с ожидаемым" + expError);
+            Assert.AreEqual(expResult.Length, actResult.Length);
+            for (int i = 0; i < expResult.Length; i++)
+            {
+                Assert.AreEqual(expResult[i], actResult[i]);
+            }
+        }
+    }
+}
